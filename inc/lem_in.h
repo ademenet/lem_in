f2889be3f4ecd0e83561f260f9bd86c29fb6169c
Alain DEMENET <ademenet@student.42.fr>
2016-05-31 19:06:00 +0200

    Update

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -6,11 +6,28 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/30 17:24:50 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/30 17:25:35 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/05/31 13:14:17 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
+
+typedef			t_liroom;
+typedef			t_litube;
+
+struct			s_liroom
+{
+	char		*name;
+	int			coord[2];
+	t_liroom	*next;
+}
+
+struct			s_litube
+{
+	t_liroom	*r1;
+	t_liroom	*r2;
+	t_litube	*next;
+}
 
 #endif
