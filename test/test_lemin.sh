3e31110b067c0c341a45619ebc8d9b5a3c88b944
Alain DEMENET <ademenet@student.42.fr>
2016-06-01 11:22:55 +0200

    Beautify

--- test/test_lemin.sh
+++ test/test_lemin.sh
@@ -7,12 +7,11 @@ then
 fi
 
 # Ask the user which test to launch.
-echo "\033[34;1mWhich test would you like to launch ? 1000 or... q to quit.\033[0m"
 while true; do
+	echo "\033[34;1mWhich test would you like to launch? 1000 or... q to quit.\033[0m"
 	read tests
 	case $tests in
 		"1000" )	./lem_in < test_1000.map ;;
 		"q" )		exit ;;
-		* )			echo "\033[34;1mPlease tell me which test you wan't: 1000, or... q to quit.\033[0m" ;;
 	esac
 done
