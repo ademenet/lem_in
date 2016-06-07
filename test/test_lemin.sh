493a03b0f71e62a9908db71ab7b33fa1c7a24853
Alain DEMENET <ademenet@student.42.fr>
2016-06-07 13:52:11 +0200

    Updating shell script tests

--- test/test_lemin.sh
+++ test/test_lemin.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+#
+# This is a shell script in order to test your lem_in program.
+#
+
+test_errors()
+{
+	for i in {1..17}
+	do
+		printf "\033[34;1m./lem_in < test/test_err%d.map\n\033[0m" $i
+		./lem_in < test/test_err$i.map
+		echo '-----'
+	done
+}
+
+test_valid()
+{
+	for i in {1..6}
+	do
+		printf "\033[34;1m./lem_in < test/test_%d.map\n\033[0m" $i
+		./lem_in < test/test_$i.map
+		echo '-----'
+	done
+}
+
 # Checking if the program exist. If not: do make.
 if [ ! -f ./lem_in ];
 then
@@ -8,16 +32,18 @@ fi
 
 # Ask the user which test to launch.
 while true; do
-	echo "\033[34;1mChoose a test: 1, 2, 1000, errors or q to quit.\033[0m"
+	echo "\033[34;1mChoose a test: 1, 2, ... 6, all, 1000, err or q to quit.\033[0m"
 	read tests
 	case $tests in
 		"1" )		./lem_in < test/test_1.map ;;
 		"2" )		./lem_in < test/test_2.map ;;
+		"3" )		./lem_in < test/test_3.map ;;
+		"4" )		./lem_in < test/test_4.map ;;
+		"5" )		./lem_in < test/test_5.map ;;
+		"6" )		./lem_in < test/test_6.map ;;
+		"all" )		test_valid ;;
 		"1000" )	./lem_in < test/test_1000.map ;;
-		"errors" )	./lem_in < test/test_err1.map
-					./lem_in < test/test_err2.map
-					./lem_in < test/test_err3.map
-					./lem_in < test/test_err4.map ;;
+		"err" )		test_errors ;;
 		"q" )		exit ;;
 	esac
 done
