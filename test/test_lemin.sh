eaee88a64db454655f263736219a89bf438cc1f4
Alain DEMENET <ademenet@student.42.fr>
2016-06-10 14:42:43 +0200

    Adding new tests

--- test/test_lemin.sh
+++ test/test_lemin.sh
@@ -4,9 +4,9 @@
 # This is a shell script in order to test your lem_in program.
 #
 
-test_errors()
+test_errors1()
 {
-	for i in {1..18}
+	for i in {1..22}
 	do
 		printf "\033[34;1m./lem_in < test/test_err%d.map\n\033[0m" $i
 		./lem_in < test/test_err$i.map
@@ -14,11 +14,42 @@ test_errors()
 	done
 }
 
-test_valid()
+test_errors2()
 {
-	for i in {1..6}
+	for i in {23..44}
+	do
+		printf "\033[34;1m./lem_in < test/test_err%d.map\n\033[0m" $i
+		./lem_in < test/test_err$i.map
+		echo '-----'
+	done
+}
+
+test_valid1()
+{
+	for i in {1..15}
 	do
 		printf "\033[34;1m./lem_in < test/test_%d.map\n\033[0m" $i
+		./lem_in < test/test_$i.map
+		echo '-----'
+	done
+}
+
+test_valid2()
+{
+	for i in {16..28}
+	do
+		printf "\033[34;1m./lem_in < test/test_%d.map\n\033[0m" $i
+		./lem_in < test/test_$i.map
+		echo '-----'
+	done
+}
+
+
+test_big()
+{
+	for i in {1..5}
+	do
+		printf "\033[34;1m./lem_in < test/test_big%d.map\n\033[0m" $i
 		./lem_in < test/test_$i.map
 		echo '-----'
 	done
@@ -32,17 +63,25 @@ fi
 
 # Ask the user which test to launch.
 while true; do
-	echo "\033[34;1mChoose a test: 1, 2, ... 6, all, 1000, err or q to quit.\033[0m"
+	echo "\033[34;1mChoose a test:\n
+		[1]\tValids from 1 to 15\n
+		[2]\tValids from 16 to 28\n
+		[3]\tErrors from 1 to 22\n
+		[4]\tErrors from 23 to 44\n
+		[5-i]\tBig with i from 1 to 5\n
+		[q]\tQuit\n
+		\033[0m"
 	read tests
 	case $tests in
-		"1" )		./lem_in < test/test_1.map ;;
-		"2" )		./lem_in < test/test_2.map ;;
-		"3" )		./lem_in < test/test_3.map ;;
-		"4" )		./lem_in < test/test_4.map ;;
-		"5" )		./lem_in < test/test_5.map ;;
-		"6" )		./lem_in < test/test_6.map ;;
-		"all" )		test_valid ;;
-		"1000" )	./lem_in < test/test_1000.map ;;
+		"1" )		test_valid1 ;;
+		"2" )		test_valid2 ;;
+		"3" )		test_errors1 ;;
+		"4" )		test_errors2 ;;
+		"5-1" )		./lem_in < test/test_big1.map ;;
+		"5-2" )		./lem_in < test/test_big2.map ;;
+		"5-3" )		./lem_in < test/test_big3.map ;;
+		"5-4" )		./lem_in < test/test_big4.map ;;
+		"5-5" )		./lem_in < test/test_big5.map ;;
 		"err" )		test_errors ;;
 		"q" )		exit ;;
 	esac
