c5a4a5085477a145f20a90f6d6558ea4a8fdb7c6
Alain DEMENET <ademenet@student.42.fr>
2016-06-30 19:26:00 +0200

    Clean everything but the first line read by get_next_line

--- src/li_clean.c
+++ src/li_clean.c
@@ -6,21 +6,18 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/28 16:37:37 by ademenet          #+#    #+#             */
-/*   Updated: 2016/06/29 14:09:14 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/06/30 19:18:19 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lem_in.h"
 
-void				li_clean_everything(t_graph *data, t_path *path)
+void				li_clean_everything(t_graph *data, t_path **paths)
 {
 	t_room			*del;
 	t_room			*to_del;
-	t_path			*delpath;
-	t_path			*to_delpath;
 
 	del = data->queue;
-	delpath = path;
 	while (del)
 	{
 		to_del = del;
@@ -29,12 +26,28 @@ void				li_clean_everything(t_graph *data, t_path *path)
 		free(to_del->tube);
 		free(to_del);
 	}
-	while (delpath)
+	li_clean_paths(paths);
+}
+
+void				li_clean_paths(t_path **paths)
+{
+	t_path			*delpath;
+	t_path			*to_delpath;
+	int				i;
+
+	i = 0;
+	while (paths[i] != NULL)
 	{
-		to_delpath = delpath;
-		delpath = delpath->next;
-		free(to_delpath);
+		delpath = paths[i];
+		while (delpath)
+		{
+			to_delpath = delpath;
+			delpath = delpath->next;
+			free(to_delpath);
+		}
+		i++;
 	}
+	free(paths);
 }
 
 void				li_clean_one_elem(t_room *to_del)
