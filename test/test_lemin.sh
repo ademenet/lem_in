b2a539337a90c2bacf6e947bc69181c4726b83c0
Alain DEMENET <ademenet@student.42.fr>
2016-07-04 18:28:27 +0200

    Add tests

--- test/test_lemin.sh
+++ test/test_lemin.sh
@@ -24,26 +24,6 @@ test_errors2()
 	done
 }
 
-test_errors1b()
-{
-	for i in {1..24}
-	do
-		printf "\033[34;1m./lem-in < test/test_err%d.map\n\033[0m" $i
-		./lem-in -v < test/test_err$i.map
-		echo '-----'
-	done
-}
-
-test_errors2b()
-{
-	for i in {25..48}
-	do
-		printf "\033[34;1m./lem-in < test/test_err%d.map\n\033[0m" $i
-		./lem-in -v < test/test_err$i.map
-		echo '-----'
-	done
-}
-
 test_valid1()
 {
 	for i in {1..15}
@@ -64,15 +44,17 @@ test_valid2()
 	done
 }
 
-
-test_big()
+test_bonus()
 {
-	for i in {1..5}
-	do
-		printf "\033[34;1m./lem-in < test/test_big%d.map\n\033[0m" $i
-		./lem-in < test/test_$i.map
-		echo '-----'
-	done
+	printf "\033[34;1m./lem-in -c < test/test_30.map\n\033[0m"
+	./lem-in -c < test/test_30.map
+	echo '-----'
+	printf "\033[34;1m./lem-in -v < test/test_err2.map\n\033[0m"
+	./lem-in -v < test/test_err2.map
+	echo '-----'
+	printf "\033[34;1m./lem-in -v < test/test_err7.map\n\033[0m"
+	./lem-in -v < test/test_err7.map
+	echo '-----'
 }
 
 # Checking if the program exist. If not: do make.
@@ -88,9 +70,8 @@ while true; do
 		[2]\tValids from 16 to 30\n
 		[3]\tErrors from 1 to 24\n
 		[4]\tErrors from 25 to 48\n
-		[3b]\tErrors from 1 to 24 with verbose mode (bonus)\n
-		[4b]\tErrors from 25 to 48 with verbose mode (bonus)\n
 		[5-i]\tBig with i from 1 to 5\n
+		[b]\tSee bonuses\n
 		[q]\tQuit\n
 		\033[0m"
 	read tests
@@ -99,14 +80,12 @@ while true; do
 		"2" )		test_valid2 ;;
 		"3" )		test_errors1 ;;
 		"4" )		test_errors2 ;;
-		"3b" )		test_errors1b ;;
-		"4b" )		test_errors2b ;;
 		"5-1" )		./lem-in < test/test_big1.map ;;
 		"5-2" )		./lem-in < test/test_big2.map ;;
 		"5-3" )		./lem-in < test/test_big3.map ;;
 		"5-4" )		./lem-in < test/test_big4.map ;;
 		"5-5" )		./lem-in < test/test_big5.map ;;
-		"err" )		test_errors ;;
+		"b" )		test_bonus ;;
 		"q" )		exit ;;
 	esac
 done
